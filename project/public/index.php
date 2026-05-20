5b512c9a7b2fb1f8b1bf33b3d3a51672858c31b6
RealDeadRevenge <v.lissakovskiy@gmail.com>
2026-05-20 19:14:17 +0500

    add base php application structure

---
+++ project/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+echo 'Application works';
